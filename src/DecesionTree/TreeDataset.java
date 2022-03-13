18e325178ff3a49d21b8e73872ea83ce60e62e31
Kazi Muktadir Ahmed <bsse1111@iit.du.ac.bd>
2022-03-13 14:15:07 +0600

    Added wine data and description file

--- src/DecesionTree/TreeDataset.java
+++ src/DecesionTree/TreeDataset.java
@@ -2,19 +2,27 @@ package DecesionTree;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class TreeDataset {
     private final String filePath;
-    public Wine[] wines;
 
-    public TreeDataset (String filePath) {
+    public ArrayList<Wine> wines = new ArrayList<>();
+    public ArrayList<ArrayList<Wine>> controlData = new ArrayList<>();
+
+    public TreeDataset (String filePath) throws FileNotFoundException {
         this.filePath = filePath;
+        parseDataFile();
     }
 
-    private void parseDataset() throws FileNotFoundException {
+    private void parseDataFile() throws FileNotFoundException {
         File dataFile = new File(filePath);
         Scanner scanDataFile = new Scanner(dataFile);
+
+        this.controlData.add(new ArrayList<Wine>());
+        this.controlData.add(new ArrayList<Wine>());
+        this.controlData.add(new ArrayList<Wine>());
 
         String[] wineData;
 
@@ -22,10 +30,17 @@ public class TreeDataset {
             String dataLine = scanDataFile.nextLine();
             wineData = dataLine.split(",", 0);
 
-            for (int i=0; i<wineData.length; i++) {
+            Wine wine = new Wine();
+            for (int i=1; i<wineData.length; i++) {
+                wine.set(i, Double.parseDouble(wineData[i]));
+            } wines.add(wine);
 
-            }
+            if (Integer.parseInt(wineData[0]) == 1)
+                this.controlData.get(0).add(wine);
+            else if (Integer.parseInt(wineData[0]) == 2)
+                this.controlData.get(1).add(wine);
+            else if (Integer.parseInt(wineData[0]) == 3)
+                this.controlData.get(2).add(wine);
         }
-
     }
 }
--- src/DecesionTree/Wine.java
+++ src/DecesionTree/Wine.java
@@ -17,32 +17,45 @@ public class Wine {
 
     public void set(int index, double value){
         switch (index) {
-            case 0:
-                this.Alchol = value;
             case 1:
-                this.MalicAcid = value;
+                this.Alchol = value;
+                break;
             case 2:
-                this.Ash = value;
+                this.MalicAcid = value;
+                break;
             case 3:
-                this.AlcalinityOfAsh = value;
+                this.Ash = value;
+                break;
             case 4:
-                this.Magnesium = value;
+                this.AlcalinityOfAsh = value;
+                break;
             case 5:
-                this.TotalPhenols = value;
+                this.Magnesium = value;
+                break;
             case 6:
-                this.Flavanoids = value;
+                this.TotalPhenols = value;
+                break;
             case 7:
-                this.NonflavanoidPhenols = value;
+                this.Flavanoids = value;
+                break;
             case 8:
-                this.Proanthocyanins = value;
+                this.NonflavanoidPhenols = value;
+                break;
             case 9:
-                this.ColorIntensity = value;
+                this.Proanthocyanins = value;
+                break;
             case 10:
-                this.Hue = value;
+                this.ColorIntensity = value;
+                break;
             case 11:
-                this.OD280_OD315_DilutedWines = value;
+                this.Hue = value;
+                break;
             case 12:
+                this.OD280_OD315_DilutedWines = value;
+                break;
+            case 13:
                 this.Proline = value;
+                break;
             default:
                 System.out.println("Wrong input");
         }
@@ -50,34 +63,64 @@ public class Wine {
 
     public double get(int index) {
         switch (index){
-            case 0:
-                return this.Alchol;
             case 1:
-                return this.MalicAcid;
+                return this.Alchol;
             case 2:
-                return this.Ash;
+                return this.MalicAcid;
             case 3:
-                return this.AlcalinityOfAsh;
+                return this.Ash;
             case 4:
-                return this.Magnesium;
+                return this.AlcalinityOfAsh;
             case 5:
-                return this.TotalPhenols;
+                return this.Magnesium;
             case 6:
-                return this.Flavanoids;
+                return this.TotalPhenols;
             case 7:
-                return this.NonflavanoidPhenols;
+                return this.Flavanoids;
             case 8:
-                return this.Proanthocyanins;
+                return this.NonflavanoidPhenols;
             case 9:
-                return this.ColorIntensity;
+                return this.Proanthocyanins;
             case 10:
-                return this.Hue;
+                return this.ColorIntensity;
             case 11:
-                return this.OD280_OD315_DilutedWines;
+                return this.Hue;
             case 12:
+                return this.OD280_OD315_DilutedWines;
+            case 13:
                 return this.Proline;
             default:
                 return -1;
         }
+    }
+
+    public boolean match(Wine wine) {
+        if (this.Alchol != wine.Alchol)
+            return false;
+        if (this.MalicAcid != wine.MalicAcid)
+            return false;
+        if (this.Ash != wine.Ash)
+            return false;
+        if (this.AlcalinityOfAsh != wine.AlcalinityOfAsh)
+            return false;
+        if (this.Magnesium != wine.Magnesium)
+            return false;
+        if (this.TotalPhenols != wine.TotalPhenols)
+            return false;
+        if (this.Flavanoids != wine.Flavanoids)
+            return false;
+        if (this.NonflavanoidPhenols != wine.NonflavanoidPhenols)
+            return false;
+        if (this.Proanthocyanins != wine.Proanthocyanins)
+            return false;
+        if (this.ColorIntensity != wine.ColorIntensity)
+            return false;
+        if (this.Hue != wine.Hue)
+            return false;
+        if (this.OD280_OD315_DilutedWines != wine.OD280_OD315_DilutedWines)
+            return false;
+        if (this.Proline != wine.Proline)
+            return false;
+        return true;
     }
 }
