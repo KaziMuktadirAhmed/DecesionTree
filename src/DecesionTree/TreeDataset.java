99dd4caec99e4ccc66b80dde64a0a2ca44b20fb2
Kazi Muktadir Ahmed <bsse1111@iit.du.ac.bd>
2022-03-14 06:01:37 +0600

    finished setting up the dataset

--- src/DecesionTree/TreeDataset.java
+++ src/DecesionTree/TreeDataset.java
@@ -3,6 +3,7 @@ package DecesionTree;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
+import java.util.Random;
 import java.util.Scanner;
 
 public class TreeDataset {
@@ -11,20 +12,26 @@ public class TreeDataset {
     public ArrayList<Wine> wines = new ArrayList<>();
     public ArrayList<ArrayList<Wine>> controlData = new ArrayList<>();
 
-    public ArrayList
+    public ArrayList<Wine> TrainingDataset = new ArrayList<>();
+    public ArrayList<Wine> TestingDataset;
 
     public TreeDataset (String filePath) throws FileNotFoundException {
         this.filePath = filePath;
+        setInitialDatasets();
+    }
+
+    private void setInitialDatasets() throws FileNotFoundException {
         parseDataFile();
+        setTrainingDataset();
     }
 
     private void parseDataFile() throws FileNotFoundException {
         File dataFile = new File(filePath);
         Scanner scanDataFile = new Scanner(dataFile);
 
-        this.controlData.add(new ArrayList<Wine>());
-        this.controlData.add(new ArrayList<Wine>());
-        this.controlData.add(new ArrayList<Wine>());
+        this.controlData.add(new ArrayList<>());
+        this.controlData.add(new ArrayList<>());
+        this.controlData.add(new ArrayList<>());
 
         String[] wineData;
 
@@ -44,5 +51,23 @@ public class TreeDataset {
             else if (Integer.parseInt(wineData[0]) == 3)
                 this.controlData.get(2).add(wine);
         }
+    }
+
+    private void setTrainingDataset () {
+        ArrayList<Wine> tempWines = wines;
+        int maximumSize = (int) Math.ceil(((double) wines.size() * 90) / (double) 100);
+
+        while (TrainingDataset.size() < maximumSize) {
+            int newRandomIndex = rangedRandom(0, tempWines.size()-1);
+            TrainingDataset.add(tempWines.get(newRandomIndex));
+            tempWines.remove(newRandomIndex);
+        }
+
+        TestingDataset = tempWines;
+    }
+
+    private int rangedRandom (int start, int end) {
+        Random rand = new Random();
+        return rand.nextInt(end - start + 1) + start;
     }
 }
