67fa11859d40736f1660101a6f5cd2800b4fcb7b
Kazi Muktadir Ahmed <bsse1111@iit.du.ac.bd>
2022-03-14 08:08:45 +0600

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	src/DecesionTree/TreeDataset.java

--- src/DecesionTree/Node.java
+++ src/DecesionTree/Node.java
@@ -1,4 +1,18 @@
 package DecesionTree;
 
+import java.util.ArrayList;
+
 public class Node {
+    public String classType;
+    public ArrayList<Wine> data;
+    public Node leftChild, rightChild;
+
+    public Node () {}
+
+    public Node(String classType, ArrayList<Wine> data, Node leftChild, Node rightChild) {
+        this.classType = classType;
+        this.data = data;
+        this.leftChild = leftChild;
+        this.rightChild = rightChild;
+    }
 }
