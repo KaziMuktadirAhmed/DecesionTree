71e27a8ba9f300ce4d882c85b5638deab806b9d9
Kazi Muktadir Ahmed <55475124+KaziMuktadirAhmed@users.noreply.github.com>
2022-03-16 15:54:14 +0600

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	src/DecesionTree/TreeDataset.java

--- src/MainPac/Driver.java
+++ src/MainPac/Driver.java
@@ -30,24 +30,25 @@ public class Driver {
 
     private double calculateAggregatedAverage() throws FileNotFoundException {
         double aggregatedAvgAccuracy = 0.0;
+        int numberOfTests = 1000;
 
-        for (int i=0; i<1000; i++) {
+        for (int i=0; i<numberOfTests; i++) {
             treeDataset = new TreeDataset("wine.data");
             decesionTree = new Tree(treeDataset);
             aggregatedAvgAccuracy += calculateAccuracy(treeDataset.TestingDataset, decesionTree);
         }
 
-        aggregatedAvgAccuracy /= 1000;
+        aggregatedAvgAccuracy /= numberOfTests;
         return aggregatedAvgAccuracy;
     }
 
-    private double calculateAccuracy (ArrayList<Wine> testSet, Tree decesionTree) {
+    private double calculateAccuracy (ArrayList<Wine> testSet, Tree decisionTree) {
         double average = 0.0;
         int totalTestCase = testSet.size();
         int totalAccurateFound = 0;
 
         for (Wine wine : testSet) {
-            if (wine.classType == decesionTree.checkWine(wine))
+            if (wine.classType == decisionTree.checkWine(wine))
                 totalAccurateFound++;
         }
 
